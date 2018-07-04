da32f7e0fb3ee448600430cb138054b2114826e3
berlin2017 <chenbolin88819@gmail.com>
2018-07-04 22:22:51 +0800

    new

---
+++ siteinfo.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "宅鲜时代测试",
+    uniacid: "6",
+    acid: "6",
+    multiid: "0",
+    version: "1.1",
+    siteroot: "https://zxsd77.cn/app/index.php",
+    design_method: "3"
+};
